ec016496b550a92296c8f69a64ea3c9bb8fdb911
Tiago Tavares <tmelotavares@gmail.com>
2020-02-10 21:22:43 -0200

    First commit

---
+++ src/test/java/br/com/tmelo/SpringBootInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tmelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
